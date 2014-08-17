6b85f287ffaaedf86e69e160873919b9561a867a
kawanamiyuu <i.am.really.you@gmail.com>
2014-08-17 23:57:59 +0900

    TRANSITIONING TO SHOW ONLY INCOMPLETE TODOS

--- src/js/router.js
+++ src/js/router.js
@@ -1,12 +1,24 @@
 Todos.Router.map(function() {
 	this.resource('todos', { path: '/' }, function () {
-		// additional child routes will go here later
+		// additional child routes
+		this.route('active');
 	});
 });
 
 Todos.TodosRoute = Ember.Route.extend({
 	model: function() {
 		return this.store.find('todo');
+	}
+});
+
+Todos.TodosActiveRoute = Ember.Route.extend({
+	model: function(){
+		return this.store.filter('todo', function(todo) {
+			return !todo.get('isCompleted');
+		});
+	},
+	renderTemplate: function(controller) {
+		this.render('todos/index', {controller: controller});
 	}
 });
 
